5c6fafba40c1916830f16ac151136937bd43ea37
刘坤 <liukunup@163.com>
2025-10-09 09:44:59 +0000

    feat(comment): 开启评论功能

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -168,17 +168,17 @@ export default defineUserConfig({
      * 评论 comments
      * @see https://theme-plume.vuejs.press/guide/features/comments/
      */
-    // comment: {
-    //   provider: '', // "Artalk" | "Giscus" | "Twikoo" | "Waline"
-    //   comment: true,
-    //   repo: '',
-    //   repoId: '',
-    //   category: '',
-    //   categoryId: '',
-    //   mapping: 'pathname',
-    //   reactionsEnabled: true,
-    //   inputPosition: 'top',
-    // },
+    comment: {
+      provider: 'Giscus', // "Artalk" | "Giscus" | "Twikoo" | "Waline"
+      comment: true,
+      repo: 'liukunup/Giscus',
+      repoId: 'R_kgDOP_Imwg',
+      category: 'Announcements',
+      categoryId: 'DIC_kwDOP_Imws4Cwbcu',
+      mapping: 'pathname',
+      reactionsEnabled: true,
+      inputPosition: 'top',
+    },
 
     /**
      * 资源链接替换
