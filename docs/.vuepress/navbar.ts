6552c77a1caf4e05b4a70a5cac0e1ddf849cc3b9
刘坤 <liukunup@163.com>
2025-10-11 02:42:58 +0000

    feat(ui): 新增导航栏图标

--- docs/.vuepress/navbar.ts
+++ docs/.vuepress/navbar.ts
@@ -7,27 +7,32 @@
 import { defineNavbarConfig } from 'vuepress-theme-plume'
 
 export default defineNavbarConfig([
-  { text: '首页', link: '/' },
-  { text: '博客', link: '/blog/' },
-  { text: '标签', link: '/blog/tags/' },
-  { text: '归档', link: '/blog/archives/' },
+  { text: '首页', link: '/', icon: 'carbon:home' },
+  { text: '博客', link: '/blog/', icon: 'carbon:blog' },
+  { text: '标签', link: '/blog/tags/', icon: 'carbon:tag' },
+  { text: '归档', link: '/blog/archives/', icon: 'carbon:archive' },
   {
     text: '笔记',
+    icon: 'carbon:notebook',
     items: [
       {
         text: 'Python',
+        icon: 'devicon:python',
         link: '/notes/python/'
       },
       {
         text: 'Go',
+        icon: 'devicon:go',
         link: '/notes/go/'
       },
       {
         text: 'Java',
+        icon: 'devicon:java',
         link: '/notes/java/'
       },
       {
         text: 'C/C++',
+        icon: 'devicon:cplusplus',
         link: '/notes/cpp/'
       },
       // {
@@ -54,6 +59,7 @@ export default defineNavbarConfig([
   },
   {
     text: '家庭实验室',
+    icon: 'icomoon-free:lab',
     items: [
       {
         text: '架构设计',
